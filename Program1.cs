91452d33d8e23f8440411a9350cbc4d099c527ad
zefania020 <85860150+zefania020@users.noreply.github.com>
2022-02-27 07:39:55 +0700

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BelajarCSHARP
+{
+	class programHW
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey(true);
+		}
+	}
+}
